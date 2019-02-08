f1b64f2a5a9b6e6d08d101266b108c1f7825338b
Georgi Kirilov <gkkirilov@gmail.com>
2019-02-08 19:26:09 +0200

    Init Timer that works

---
+++ facilitatortimer/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Timer :time="300"></Timer>
+  </div>
+</template>
+
+<script>
+import Timer from './components/Timer'
+
+export default {
+  name: 'app',
+  components: {
+    Timer
+  },
+  data() {
+    return {
+      time: 300
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ facilitatortimer/src/components/Timer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <span class="timer">
+      {{timeLeft}}
+    </span>
+    <!-- <button @click="startTimer">Start Timer</button> -->
+    <button @click="moreTime">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Timer',
+  props: {
+    time: Number
+  },
+  data() {
+    return {
+      startTS: Date.now(),
+      timerLength: 300,
+      timeLeft: 300
+    }
+  },
+  computed: {
+    endTS: function () {
+      return this.startTS + this.timerLength * 1000;
+    }
+  },
+  methods: {
+    startTimer: function() {
+      this.timer = setInterval(() => this.countdown(), 1000);
+    },
+    moreTime: function () {
+      this.totalLength+=60
+    },
+    countdown: function() {
+      this.timeLeft = ((this.endTS - Date.now())/1000).toFixed(0);//+this.timerLength
+    }
+  },
+  mounted() {
+    this.startTimer();
+  },
+}
+</script>
+
+<style>
+.timer{font-size:10rem}
+</style>
