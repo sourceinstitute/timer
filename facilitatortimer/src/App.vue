302809dc572e66f188fdac960980760c67ee4d01
Salim Virani <smile@saintsal.com>
2019-02-26 13:43:58 +0200

    font and layout tweaks

--- facilitatortimer/src/App.vue
+++ facilitatortimer/src/App.vue
@@ -17,7 +17,8 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  @import url('https://fonts.googleapis.com/css?family=Oxygen:400');
+  font-family: "Oxygen", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
